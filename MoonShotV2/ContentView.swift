d4bc191b114a71d9163c66efe915d275311b9290
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-03-02 15:59:39 -0800

    CustomText struct written and implemented

--- MoonShotV2/ContentView.swift
+++ MoonShotV2/ContentView.swift
@@ -7,16 +7,31 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    var text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         ScrollView(.vertical) {
             VStack(spacing: 10) {
-                ForEach(0..<100) {
-                    Text("Item \($0)")
-                        .font(.title)
-                }
+                
+                    ForEach(0..<100) {
+                        CustomText("Item \($0)")
+                            .font(.title)
+                    }
+                .frame(maxWidth: .infinity)
             }
-            .frame(maxWidth: .infinity)
+            
         }
     }
 }
