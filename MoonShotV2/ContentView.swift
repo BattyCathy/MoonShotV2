22b9852d0b270844fb5cbdca590e22e44942614f
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-03-02 15:46:11 -0800

    ScrollView implemented

--- MoonShotV2/ContentView.swift
+++ MoonShotV2/ContentView.swift
@@ -9,12 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            GeometryReader { geo in
-                Image("Example")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: geo.size.width)
+        ScrollView(.vertical) {
+            VStack(spacing: 10) {
+                ForEach(0..<100) {
+                    Text("Item \($0)")
+                        .font(.title)
+                }
             }
         }
     }
