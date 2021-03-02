1d30a3f375d2717556dbec730b7f639fd84a923b
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-03-02 14:00:38 -0800

    Added TOC + Prologue

---
+++ MoonShotV2/Notes/00. Prologue.swift
@@ -0,0 +1,36 @@
+//
+//  00. Prologue.swift
+//  MoonShotV2
+//
+//  Created by Austin Roach on 3/2/21.
+//
+
+import Foundation
+
+//MARK: 100 Days of SwiftUI, Day 39
+
+//MARK: Project 8, part 1
+
+//As is write these words, today is the day Apple TV+ launches - a new subscription service that promises high-quiality streaming television with a whole new raft of original content.
+
+//In my filter bubble, the show everyone is talking about is "For All Mankind" that dramatizes an alternative history of the 1969 moon landing. So, I figured today would be a good day to start a SwiftUI project called Moonshot, detailing some of the history behind NASA's Apollo space program.
+
+//I also think it would be fitting if today's quote came from Neil Arstrong, the first person to walk on the moon. Back in 2000 he said, "science is about what it is; engineering is about what can be." I don't know about you, but I find that hugely inspiring. Every time we create a new Xcoder project we have a blank slate to work with, and that can be whatever we want.
+
+//Today we're learning about the techniques to build Moonshot, but as with all the techniques we're learining they form part of your larger knowledge for you to mix and remix as you please for years to come.
+
+//Today you have five topics to work through, in whihc you'll learn about GeometryReader, ScrollView, NavigationLink, and more.
+
+//1. Moonshot: Introduction
+
+//2. Resizing images to fit the screen using GeometryReader
+
+//3. How ScrollView lets us work with scrolling data
+
+//4. Pushing new views onto the stack using NavigationLink
+
+//5. Working with hierarchical Codable data
+
+//Don't forget to post your progress somewhere - stay accountable! (And when your done, sit down and watch some For All Mankind.)
+
+
