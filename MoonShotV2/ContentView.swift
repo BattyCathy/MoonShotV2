0fcfd011af6dc94fab0f4baea7908694b5cb6472
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-03-02 13:45:15 -0800

    Initial Commit

---
+++ MoonShotV2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MoonShotV2
+//
+//  Created by Austin Roach on 3/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MoonShotV2/MoonShotV2App.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotV2App.swift
+//  MoonShotV2
+//
+//  Created by Austin Roach on 3/2/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
