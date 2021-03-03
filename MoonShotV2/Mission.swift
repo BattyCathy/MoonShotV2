82cb905c5d49af9c4af9fca84a3fbecba118b4a1
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-03-02 18:58:04 -0800

    Mission struct added

--- MoonShotV2/Mission.swift
+++ MoonShotV2/Mission.swift
@@ -11,3 +11,10 @@ struct CrewRole: Codable {
     let name: String
     let role: String
 }
+
+struct Mission: Codable, Identifiable {
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
