25a18932e192caa6ef0451cae14fb0f5636f13f9
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-03-02 16:04:01 -0800

    Notes 04. Pushing new views onto the stack using NavigationLink file added

---
+++ MoonShotV2/Notes/04. Pushing New Views Onto the Stack Using NavigationLink.swift
@@ -0,0 +1,15 @@
+//
+//  04. Pushing New Views Onto the Stack Using NavigationLink.swift
+//  MoonShotV2
+//
+//  Created by Austin Roach on 3/2/21.
+//
+
+import Foundation
+
+
+//MARK: 04. Pushing New Views Onto The Stack Using NavigationLink
+
+//SwiftUI's NavigationView shows a navigation bar at the top of our views, but also does something else: it lets us push views onto a view stack. In fact, this is really the most fundamental form of iOS navigation - you can see it in Settings when you tap Wifi of General, or in Messages whenver you tap someone's name.
+
+//This view stack system is very different from the sheets we've used preivously. Yes, both show some sort of new view, but there's a difference in the way they are presented that affects the ways users think about them. 
