92f63df59727fec241871f193e4422a32dc5082d
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-03-02 18:07:17 -0800

    Bundle-Decodable created

---
+++ MoonShotV2/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShotV2
+//
+//  Created by Austin Roach on 3/2/21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
