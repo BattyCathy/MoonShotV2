1b07b4837ca9c0c778a8152860a8f3e67acdcb4c
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-03-02 17:16:56 -0800

    TOC Transcribed

---
+++ MoonShotV2/Notes/Day Two/00. TOC.swift
@@ -0,0 +1,26 @@
+//
+//  00. TOC.swift
+//  MoonShotV2
+//
+//  Created by Austin Roach on 3/2/21.
+//
+
+import Foundation
+
+//MARK: 100 Days of SwiftUI: Day 40
+
+//Project 8, part 2
+
+//Today we're going to be working through the first part of our application, and in particular we're going to be getting our data right. I realize that data isn't terribly exciting, but as your skills grow you'll start to see how data realy defines what your app can do - no matter what dancy designs you add or what slick animations you introduct, if you data isn't right your app just won't work.
+
+//Along the way you're going to meet an important Swift feature called generics. This is a feature I definitly class as well outside of bignner Swift, but as you'll see generic let us create highly reusable code with only a little extra thinking.
+
+//Reusable code is important, becuase it helps us accomplish bigger and better results with less work. However, as Ralph Johnson once said, "before software can be resuable it first has to be usable" - as nice as generic are, we'll only start using them once we've soved the problem the easier way first.
+
+//Today you have three topics to work through, in whihc you'll get more hands-on experience with Codable, take genereics for a test drive, and more.
+
+// 01. Loading a specific kind of Codable data
+
+// 02. Using Generics to load any kind of Codable data
+
+// 03. Formatting our mission view
