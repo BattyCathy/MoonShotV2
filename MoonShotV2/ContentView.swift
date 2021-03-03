ffcc338e4c2edfdf9f3eec1fe6e9cf49caddce30
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-03-02 19:59:11 -0800

    Mission view added to ContentView

--- MoonShotV2/ContentView.swift
+++ MoonShotV2/ContentView.swift
@@ -12,7 +12,22 @@ struct ContentView: View {
     let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     var body: some View {
-        Text("\(astronauts.count)")
+        NavigationView {
+            List(missions) { mission in
+                NavigationLink(destination: Text("Detail view")) {
+                    Image(mission.image)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 44, height: 44)
+                    
+                    VStack(alignment: .leading) {
+                        Text(mission.displayName)
+                            .font(.headline)
+                        Text(mission.launchDate ?? "N/A")
+                    }
+                }
+            }
+        }
     }
 }
 
