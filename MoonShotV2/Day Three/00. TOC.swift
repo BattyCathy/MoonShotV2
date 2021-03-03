b51ee8ad2801a2766154bd25d8951da08b0273e2
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-03-03 12:52:24 -0800

    TOC transcribed

--- MoonShotV2/Day Three/00. TOC.swift
+++ MoonShotV2/Day Three/00. TOC.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+//MARK: 100 Days of SwiftUI
+
+//MARK: Day 41
+
+//MARK: Project 8, Part 3
+
+//Today we're going to be completing our Moonshot app by adding two more views plus navigation between them, but here is where you'll start to see what it takes to create custom layouts in SwiftUI - we'll be using GeometryReader to get the view size, layoutPriority() to help guide SwiftUI's layout engine, and more.
+
+//Along the way, we're also going to be tackling one of the common problems you'll face as a programmer: when you have two pieces of separate data that need to be merged somehow. For us, that's our astronaut and mission data, but the concept is transferrable enough as you'll see.
+
+//As one point in today's topics I encourage you to stop and play around with the design. I'm sure some folks will skip over this wanting to rush towards the end, but I hope you don't. As the astronaut John Glenn said, "I suppose the quality in an astronaut more powerful than any other is curiosity - thay have to get to some place nobody's ever been."
+
+//Be curious - take the time to explore your skills and it willl pay off!
+
+//Today you have three topics to work through, in which you'll work with GeometryReader, ScrollView, layoutPriority(), and more.
+
+// 01. Showing mission details in ScrollView and GeometryReader
+
+// 02. Merging Codable structs using first(where:)
+
+// 03. Fixing problems with buttonStyle() and layoutPriority()
+
+//That's another complete app built - make sure and share your progress to let others know how you're getting on!
