a3c6983640440870b8d9c791988b2100b8f76a16
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-03-03 13:16:22 -0800

    MissionView SwiftUI view created

---
+++ MoonShotV2/MissionView.swift
@@ -0,0 +1,20 @@
+//
+//  MissionView.swift
+//  MoonShotV2
+//
+//  Created by Austin Roach on 3/3/21.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MissionView_Previews: PreviewProvider {
+    static var previews: some View {
+        MissionView()
+    }
+}
