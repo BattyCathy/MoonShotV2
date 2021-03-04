7e456e06421d32dcd04f02b7292f536c80eea2b0
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-03-03 16:44:06 -0800

    Trying on other mac

--- MoonShotV2/MissionView.swift
+++ MoonShotV2/MissionView.swift
@@ -16,6 +16,21 @@ struct MissionView: View {
         let astronaut: Astronaut
     }
     
+    init(mission: Mission, astronauts: [Astronaut]) {
+        self.mission = mission
+        
+        var matches = [CrewMember]()
+        
+        for member in mission.crew {
+            if let match = astronauts.first(where: { $0.id == member.name }) {
+                matches.append(CrewMember(role: member.role, astronaut: match))
+            } else {
+                fatalError("Missing \(member)")
+            }
+        }
+        self.astronauts = matches
+    }
+    
     var body: some View {
         GeometryReader { geometry in
             ScrollView(.vertical) {
@@ -55,22 +70,6 @@ struct MissionView: View {
             }
         }
         navigationBarTitle(Text(mission.displayName), displayMode: .inline)
-    }
-    
-    init(mission: Mission, astronauts: [Astronaut]) {
-        self.mission = mission
-        
-        var matches = [CrewMember]()
-        
-        for member in mission.crew {
-            if let match = astronauts.first(where: { $0.id == member.name}) {
-                matches.append(CrewMember(role: member.role, astronaut: match))
-            } else {
-                fatalError("Missing \(member)")
-            }
-        }
-        
-        self.astronauts = matches
     }
 }
 
